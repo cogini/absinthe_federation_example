020a9d41795e32807049ba40d84facef6c61d290
Jake Morrison <jake@cogini.com>
2024-02-12 19:21:07 -0600

    Configure CSP

--- lib/absinthe_federation_example_web/router.ex
+++ lib/absinthe_federation_example_web/router.ex
@@ -7,7 +7,7 @@ defmodule AbsintheFederationExampleWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, html: {AbsintheFederationExampleWeb.Layouts, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'"}
   end
 
   pipeline :api do
